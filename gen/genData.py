9950de152fb962469d3a5f71efc3aaf0f3a74019
tuan <theiron1997@gmail.com>
2016-11-16 00:37:33 +0700

    change BORDER_REPLICATE in warpAffine

--- gen/genData.py
+++ gen/genData.py
@@ -7,20 +7,23 @@ def genOneImage(srcImage, angle, scale, transX, transY, dstSave, name):
 
     imgSrc = cv2.imread(srcImage, 0)
     row,col = imgSrc.shape
+
     center = tuple(np.array([row,col])/2)
     matAngle = cv2.getRotationMatrix2D(center, angle, scale)
-    dstAngle = cv2.warpAffine(imgSrc, matAngle, (col, row), None, 1, 1, 1)
+    dstAngle = cv2.warpAffine(imgSrc, matAngle, (col, row), None, 1, 1, 0) # 0 is BORDER_CONSTANT, don't use BORDER_REPLICATE value 1
+                                                                        # row or col with BORDER_REPLICATE will replicate to the extra bordet
 
     matTrans = np.float32([[1, 0, transX], [0, 1, transY]])
-    dstTrans = cv2.warpAffine(dstAngle, matTrans, dstAngle.shape, None, 1, 1, 1)
+    dstTrans = cv2.warpAffine(dstAngle, matTrans, dstAngle.shape, None, 1, 1, 0)
 
-    imgDst = cv2.resize(dstTrans, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+    imgDst = cv2.resize(dstAngle, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+    #imgDst = dstTrans
     cv2.imwrite(dstSave + "/image{}.png".format(name), imgDst)
 
 def genImage(labels, listPathFiles, numImages, fileLocation) :
 
     angle = [i for i in range(-15, 15)]
-    scale = [i for i in range(80, 120)]
+    scale = [i for i in range(80, 114)]
     tmp = cv2.imread(listPathFiles[0][0], 0)
     print tmp.shape
     transX = [i for i in range(-int(tmp.shape[0] / 6), int(tmp.shape[0] / 6))]
--- main.py
+++ main.py
@@ -2,7 +2,12 @@ from gen.getTreeFile import getTreeFile
 from gen.genData import genImage
 import cv2
 
-listPathFiles, labels = getTreeFile('imageout')
-tmp = cv2.imread(listPathFiles[0][0], 0)
-genImage(labels, listPathFiles, 700, 'train')
-genImage(labels, listPathFiles, 300, 'test')
+#listPathFiles, labels = getTreeFile('imageout')
+#tmp = cv2.imread(listPathFiles[0][0], 0)
+#genImage(labels, listPathFiles, 700, 'train')
+#genImage(labels, listPathFiles, 300, 'test')
+img = cv2.imread("/home/tuan/Desktop/Katakana/test/0/image0.png")
+if img.data is None:
+    print "Cannot read image";
+print cv2.BORDER_CONSTANT
+print img
